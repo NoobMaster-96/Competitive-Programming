c8261becf1bb89265b78b27c04c6e806ceb78295
Avinash Choudhary <55015213+Avi-nash5@users.noreply.github.com>
2019-12-29 13:57:51 +0530

    Add files via upload

--- Bit manipulation/commonbittask.cpp
+++ Bit manipulation/commonbittask.cpp
@@ -5,8 +5,18 @@ bool isOdd(int n){
 	return(n&1);
 }
 
-int getBit(int n, int i){
-	return 0;
+int getBit(int n, int i){            //returns ith bit of a number
+	int a = (1<<i);
+	int bit = (n&a)>0 ? 1:0;
+
+	return bit;
+}
+
+int setBit(int n, int i){          //set the ith bit of a num to 1
+	int a = (1<<i);
+	n = (n|a);
+
+	return n;
 }
 
 int main(){
