33e2eb9fe97877947ef9e5824c2298e2a4f8ebf5
Avinash Choudhary <55015213+Avi-nash5@users.noreply.github.com>
2020-07-04 18:35:19 +0530

    Add files via upload

--- Graphs/CycleDetectionInDirectedGraph.cpp
+++ Graphs/CycleDetectionInDirectedGraph.cpp
@@ -2,64 +2,39 @@
 using namespace std;
 #define endl "\n"
 
-class graph{
-	vector<int> *adjlist;
-	int N;
+int n,e;
+vector<int> g[300];
 
-public:
-	graph(int n){
-		N = n;
-		adjlist = new vector<int>[N];
-	}
-
-	void addedge(int u, int v, bool bidir=false){
-		adjlist[u].push_back(v);
-		if(bidir){
-			adjlist[v].push_back(u);
+bool helper(int node, vector<bool> &vis, vector<bool> &s){
+	vis[node] = true;
+	s[node] = true;
+	for(auto nbr:g[node]){
+		if(s[nbr]){
+			return true;
 		}
-	}
-
-	bool cycle_helper(int node, bool *visited, bool *s){
-		visited[node] = true;
-		s[node] = true;
-		for(auto nbr:adjlist[node]){
-			if(s[nbr]==true){
+		else if(!vis[nbr]){
+			bool ans = helper(nbr,vis,s);
+			if(ans){
 				return true;
 			}
-			else if(!visited[nbr]){
-				bool cycle = cycle_helper(nbr,visited,s);
-				if(cycle){
-					return true;
-				}
-			}
 		}
-		s[node] = false;
-		return false;
 	}
+	s[node] = false;
+	return false;
+}
 
-	bool containsCycle(){
-		bool *visited = new bool[N];
-		bool *s = new bool[N];
-		for(int i=0;i<N;i++){
-			visited[i] = false;
-			s[i] = false;
-		}
-		return cycle_helper(0,visited,s);
-	}
-};
+bool containsCycle(){
+	vector<bool> vis(n,false);
+	vector<bool> s(n,false);
+	return helper(0,vis,s);
+}
 
 int main(){
-	int t;
-	cin>>t;
-	while(t--){
-		int n,e;
-		cin>>n>>e;
-		graph g(n);
-		for(int i=0;i<e;i++){
-			int u,v;
-			cin>>u>>v;
-			g.addedge(u,v);
-		}
-		cout<<g.containsCycle()<<endl;
+	cin>>n>>e;
+	for(int i=0;i<e;i++){
+		int u,v;
+		cin>>u>>v;
+		g[u].push_back(v);
 	}
+	cout<<containsCycle()<<endl;
 }
--- Graphs/StronglyConnectedComponents.cpp
+++ Graphs/StronglyConnectedComponents.cpp
@@ -4,85 +4,64 @@ using namespace std;
 
 //kosaraju's algorithm
 
-class graph{
-	int N;
-	vector<int> *adjlist;
+vector<int> g[300];
+vector<int> grr[300];
 
-public:
-	graph(int n){
-		adjlist = new vector<int>[n];
-		N = n;
-	}
-
-	void addedge(int u, int v, bool bidir=false){
-		adjlist[u].push_back(v);
-		if(bidir){
-			adjlist[v].push_back(u);
+void dfs(int node, vector<bool> &vis, vector<int> &order){
+	vis[node] = true;
+	for(auto nbr:g[node]){
+		if(!vis[nbr]){
+			dfs(nbr,vis,order);
 		}
 	}
+	order.push_back(node);
+}
 
-	void print(){
-		for(int i=0;i<N;i++){
-			cout<<i<<"->";
-			for(auto entry:adjlist[i]){
-				cout<<entry<<" ";
-			}
-			cout<<endl;
+void r_dfs(int node, int col, vector<bool> &vis, vector<int> &cmp){
+	vis[node] = true;
+	cmp[node] = col;
+	for(auto nbr:grr[node]){
+		if(!vis[nbr]){
+			r_dfs(nbr,col,vis,cmp);
 		}
 	}
-
-	void dfs(int node, vector<int> adjlist, vector<int> &ordering, vector<bool> &visited){
-		visited[node] = true;
-		for(auto nbr:adjlist[node]){
-			if(!visited[nbr]){
-				dfs(nbr,ordering,visited);
-			}
-		}
-		ordering.push_back(node);
-	}
-
-	void reverse_dfs(int node, int col, vector<int> &cmp, vector<bool> &visited){
-		visited[node] = true;
-		for(auto nbr:radjlist[node]){
-			if(!visited[nbr]){
-				dfs(nbr,ordering,visited);
-			}
-		}
-	}
-
-	int kosaraju(int n){
-		vector<int> radjlist[n];
-		vector<int> order;
-		vector<bool> vis(n,0);
-		vector<int> cmp(n,0);
-		for(int i=0;i<n;i++){
-			for(auto j:adjlist[i]){
-				radjlist[j].push_back(i);
-			}
-		}
-		for(int i=0;i<n;i++){
-			if(!vis[i]){
-				dfs(i,order,vis);
-			}
-		}
-		for(int i=0;i<n;i++){
-			vis[i] = false;
-		}
-
-	}
-
-};
+}
 
 int main(){
-	int n;
-	cin>>n;
-	graph g(n);
-	int e;
-	cin>>e;
-	while(e--){
-		int u,v;
-		cin>>u>>v;
-		g.addedge(u,v);
+	
+	int n,x;
+	cin>>n>>x;
+	while(x--){
+		int a,b;
+		cin>>a>>b;
+		g[a].push_back(b);
+		grr[b].push_back(a);
 	}
-	cout<<g.kosaraju(0)<<endl;
+	vector<bool> vis(n,0);
+	vector<int> order;
+	for(int i=0;i<n;i++){
+		if(!vis[i]){
+			dfs(i,vis,order);
+		}
+	}
+	for(int i=0;i<n;i++){
+		vis[i] = false;
+	}
+	for(int i=0;i<n;i++){
+		cout<<order[i]<<" ";
+	}
+	cout<<endl;
+	int col = 0;
+	vector<int> cmp(n,-1);
+	for(int i=n-1;i>=0;i--){
+		if(!vis[order[i]]){
+			r_dfs(order[i],col,vis,cmp);
+			col++;
+		}
+	}
+	cout<<col<<endl;
+	for(int i=0;i<n;i++){
+		cout<<cmp[i]<<" ";
+	}
+	cout<<endl;
 }
